706874134ee800c67df41156805752fb41d94dc4
Chet Kener <Cl3Kener@gmail.com>
2014-11-04 21:17:30 -0500

    Disable Sample APNs.
    * I see a warning here and no need for these
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -20,7 +20,12 @@
 # entirely appropriate to inherit from for on-device configurations.
 
 PRODUCT_PROPERTY_OVERRIDES := \
-    keyguard.no_require_sim=true
+
+    keyguard.no_require_sim=true \
+    ro.com.android.dataroaming=false
+
+PRODUCT_COPY_FILES := \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
