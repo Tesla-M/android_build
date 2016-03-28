0febfb2a4d8b04fd0a6caf2dffeed354e5cccd13
Alectradar <gokulbalram@gmail.com>
2016-03-29 00:12:28 +0530

    Brunch fixed

--- envsetup.sh
+++ envsetup.sh
@@ -532,7 +532,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        mka tesla
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
