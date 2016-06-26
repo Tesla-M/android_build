c6a9e24327bcbb5bef39ccf1bdabf4992fd9d2af
airlessproject <vlatko.mitreski@gmail.com>
2016-06-27 00:51:46 +0200

    Revert "Remove bootclasspath handling for Jack"
    This reverts commit c503620eba1a2c090e1c88ef4e196abb3e986760.

--- core/host_dalvik_static_java_library.mk
+++ core/host_dalvik_static_java_library.mk
@@ -53,5 +53,6 @@ $(full_classes_jack): $(java_sources) $(java_resource_sources) $(full_jack_lib_d
 	@echo Building with Jack: $@
 	$(java-to-jack)
 
+USE_CORE_LIB_BOOTCLASSPATH :=
 LOCAL_IS_STATIC_JAVA_LIBRARY :=
 endif
